87d50b9e0fae61e582a26c360e26b6c52e34d440
Linyxus <yichen.x@outlook.com>
2021-11-28 18:12:01 +0000

    Stage changes

--- main_hg.py
+++ main_hg.py
@@ -1,6 +1,7 @@
 import argparse
 
 import torch
+from torch.utils.data import DataLoader
 from transformers import T5ForConditionalGeneration, AutoTokenizer
 from datasets import load_dataset
 
@@ -12,15 +13,16 @@ DATASET_CONFIG = {
 }
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--device', type=str, default='cuda:0')
-    parser.add_argument('--model_name', type=str, default='google/byt5-small')
-    parser.add_argument('--dataset', type=str, default='gem-xsum')
-    args = parser.parse_args()
+DATASET_FIELDS = {
+    'gem-xsum': {
+        'input': 'document',
+        'target': 'target'
+    }
+}
 
+
+def main(args: argparse.Namespace):
     print_dict(args.__dict__, name="Command-Line Arguments")
-
     device = torch.device(args.device)
 
     print('loading dataset')
@@ -33,8 +35,33 @@ if __name__ == '__main__':
 
     model = model.to(device)
 
+    def tokenize(samples):
+        text_field = DATASET_FIELDS[args.dataset]['input']
+        target_field = DATASET_FIELDS[args.dataset]['target']
+        model_inputs = tokenizer(samples[text_field], padding="longest", truncation=True)
+        labels = tokenizer(samples[target_field], padding="longest", truncation=True).input_ids
+        return {
+            **model_inputs,
+            'labels': labels
+        }
+
+    print('preprocessing dataset')
+    preprocessed_dataset = dataset.map(tokenize, batched=True, num_proc=64)
+    train_dataset, test_dataset, val_dataset = [preprocessed_dataset[x] for x in ['train', 'test', 'validation']]
+
     model_inputs = tokenizer(["Translate English to Frensh: Life is like a box of chocolates.", "Today is Monday."], padding="longest", return_tensors="pt").to(device)
     labels = tokenizer(["La vie est comme une boîte de chocolat.", "Aujourd'hui c'est lundi."], padding="longest", return_tensors="pt").input_ids.to(device)
 
     loss = model(**model_inputs, labels=labels).loss
     print(loss)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--device', type=str, default='cuda:0')
+    parser.add_argument('--model_name', type=str, default='google/byt5-small')
+    parser.add_argument('--dataset', type=str, default='gem-xsum')
+    parser.add_argument('--batch_size', type=int, default=200)
+    args = parser.parse_args()
+
+    main(args)
--- utils.py
+++ utils.py
@@ -3,6 +3,11 @@ from datasets import load_dataset
 
 def print_dict(d: dict, name: str = None):
     if name is not None:
-        print(f'===== {name} =====')
+        s = f'===== {name} ====='
+        print(s)
+
     for k, v in d.items():
         print(f'{k} = {v}')
+
+    if name is not None:
+        print(''.join(['=' for _ in range(len(s))]))
