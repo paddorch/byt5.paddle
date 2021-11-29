51b8df9112bcb5c3d315a1f634bedad94bfb507f
Linyxus <yichen.x@outlook.com>
2021-11-29 07:58:59 +0000

    Add evaluation script

--- main_hg.py
+++ main_hg.py
@@ -2,10 +2,11 @@ import argparse
 from tqdm import tqdm
 
 import torch
+import torch.nn as nn
 from torch.utils.data import DataLoader
 from transformers import T5ForConditionalGeneration, AutoTokenizer
 from transformers.data.data_collator import DataCollatorWithPadding
-from datasets import load_dataset
+from datasets import load_dataset, load_metric
 
 from utils import print_dict
 
@@ -23,26 +24,40 @@ DATASET_FIELDS = {
 }
 
 
-def train(model: torch.nn.Module, device, optimizer: torch.optim.Optimizer, 
-          train_loader: DataLoader, val_loader: DataLoader):
+def train(model: torch.nn.Module, optimizer: torch.optim.Optimizer, 
+          train_loader: DataLoader, epoch: int):
     model.train()
     tot_loss = 0.0
     tot_samples = 0
 
-    for batch in train_loader:
-        optimizer.zero_grad()
+    with tqdm(total=len(train_loader), desc=f'[TRAIN] epoch {epoch:05d}') as pbar:
+        for batch in train_loader:
+            optimizer.zero_grad()
 
-        batch = {k: v.to(device) for k, v in batch.items()}
-        outputs = model(**batch)
-        loss = outputs.loss
-        loss.backward()
+            # batch = {k: v.to(device) for k, v in batch.items()}
+            outputs = model(**batch)
+            loss = outputs.loss
+            loss = loss.mean()
+            loss.backward()
 
-        optimizer.step()
+            optimizer.step()
 
-        tot_loss += loss.item()
-        tot_samples += batch['input_ids'].size()[0]
+            tot_loss += loss.item()
+            tot_samples += batch['input_ids'].size()[0]
+            pbar.update(1)
+            pbar.set_postfix({'loss': f'{tot_loss / tot_samples:.4f}'})
 
     return tot_loss / tot_samples
+
+
+def evaluate(model: torch.nn.Module, tokenizer: AutoTokenizer, eval_loader: DataLoader, metric):
+    model.eval()
+    device = list(model.parameters())[0].data.device
+    for batch in eval_loader:
+        batch = {k: v.to(device) for k, v in batch.items()}
+        outputs = model.generate(batch['input_ids'], max_length=1024)
+        predicted = [tokenizer.decode(x, skip_special_tokens=True) for x in outputs]
+        references = [tokenizer.decode(x, skip_special_tokens=True) for x in batch['labels'].cpu().tolist()]
 
 
 def main(args: argparse.Namespace):
@@ -66,7 +81,8 @@ def main(args: argparse.Namespace):
         text_field = DATASET_FIELDS[args.dataset]['input']
         target_field = DATASET_FIELDS[args.dataset]['target']
         model_inputs = tokenizer(samples[text_field], max_length=args.max_source_length, padding="max_length", truncation=True)
-        labels = tokenizer(samples[target_field], max_length=args.max_source_length, padding="max_length", truncation=True).input_ids
+        with tokenizer.as_target_tokenizer():
+            labels = tokenizer(samples[target_field], max_length=args.max_source_length, padding="max_length", truncation=True).input_ids
         return {
             **model_inputs,
             'labels': labels
@@ -89,24 +105,31 @@ def main(args: argparse.Namespace):
 
     optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
 
-    model_inputs = tokenizer(["Translate English to Frensh: Life is like a box of chocolates.", "Today is Monday."], padding="longest", return_tensors="pt").to(device)
-    labels = tokenizer(["La vie est comme une boîte de chocolat.", "Aujourd'hui c'est lundi."], padding="longest", return_tensors="pt").input_ids.to(device)
+    metric = load_metric('bleu')
 
-    loss = model(**model_inputs, labels=labels).loss
-    print(loss)
+    unwrapped_model = model
+    model = nn.DataParallel(model)
+
+    evaluate(unwrapped_model, tokenizer, val_loader, metric)
+
+    # model_inputs = tokenizer(["Translate English to Frensh: Life is like a box of chocolates.", "Today is Monday."], padding="longest", return_tensors="pt").to(device)
+    # labels = tokenizer(["La vie est comme une boîte de chocolat.", "Aujourd'hui c'est lundi."], padding="longest", return_tensors="pt").input_ids.to(device)
+
+    # loss = model(**model_inputs, labels=labels).loss
+    # print(loss)
 
     for epoch in range(args.num_epochs):
-        loss = train(model, device, optimizer, train_loader, val_loader)
+        loss = train(model, optimizer, train_loader, epoch)
 
         print(f'epoch {epoch:05d}, loss {loss:.8f}')
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--device', type=str, default='cuda:0')
+    parser.add_argument('--device', type=str, default='cuda')
     parser.add_argument('--model_name', type=str, default='google/byt5-small')
     parser.add_argument('--dataset', type=str, default='gem-xsum')
-    parser.add_argument('--batch_size', type=int, default=8)
+    parser.add_argument('--batch_size', type=int, default=16)
     parser.add_argument('--lr', type=float, default=0.001)
     parser.add_argument('--num_epochs', type=int, default=1000)
     parser.add_argument('--max_source_length', type=int, default=1024)
